96f80e40440523ee02178bd217447199aa600da9
D3strophe <58573091+D3strophe@users.noreply.github.com>
2022-04-30 14:36:26 -0400

    base structure researched

--- gather_tokens.r
+++ gather_tokens.r
@@ -17,3 +17,9 @@ page <- read_html("https://www.reddit.com/r/funny")
 postTime <- page %>%
   html_nodes("._3jOxDPIQ0KaOWpzvSQo-1s") %>%
   html_text()
+page <- read_html(doc_url)
+
+page %>% 
+  html_nodes("*") %>% 
+  html_attr("class") %>% 
+  unique()
