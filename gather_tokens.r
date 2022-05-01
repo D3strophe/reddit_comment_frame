33fcc70f39bfdf89569a2b77b3536b0ba8356511
D3strophe <58573091+D3strophe@users.noreply.github.com>
2022-05-01 14:04:31 -0400

    added function to gather dataframe, and function to clean data frame
    kept these two separate for user options

--- gather_tokens.r
+++ gather_tokens.r
@@ -2,14 +2,39 @@ library(dplyr)
 library(tm)
 library(RedditExtractoR)
 
-reddit_comments_dataframe <- function(input_url) {
+reddit_raw_dataframe <- function(input_url) {
+  stopifnot(is.character(input_url) && is.logical(clean))
+  
   holding <- get_thread_content(urls = input_url)
-  holding <- cbind.data.frame(holding$comments$timestamp, holding$comments$comment, holding$comments$score) 
+  holding <- cbind.data.frame(holding$comments$timestamp, holding$comments$author, holding$comments$comment, holding$comments$score)
+  
   return(holding)
 }
 
+reddit_clean_dataframe <- function(dataframe) {
+   dataset %>% rename("timestamp" = "holding$comments$timestamp", "commentor" = "holding$comments$author", "comment_content" = "holding$comments$comment", "upvotes" = "holding$comments$score") %>%
+     mutate(comment_content = tolower(comment_content))
+  
+   dataset$comment_content <- removeWords(clean_df$comment_content, stopwords("en"))
+   dataset$comment_content <- removeNumbers(clean_df$comment_content)
+   dataset$comment_content <- removePunctuation(clean_df$comment_content)
+   
+   return(dataset)
+}
+
+# reddit_comments_dataframe <- function() {
+# 
+# }
+
+
+
 dataset <- reddit_comments_dataframe("https://www.reddit.com/r/starcitizen/comments/ufuemg/317_has_made_star_citizen_feel_more_like_a_game/")
 
-#clean_df <- dataset %>%
+clean_df <- dataset %>% 
+  rename("timestamp" = "holding$comments$timestamp", "commentor" = "holding$comments$author", "comment_content" = "holding$comments$comment", "upvotes" = "holding$comments$score") %>%
+  mutate(comment_content = tolower(comment_content))
   
-  
+clean_df$comment_content <- removeWords(clean_df$comment_content, stopwords("en"))
+clean_df$comment_content <- removeNumbers(clean_df$comment_content)
+clean_df$comment_content <- removePunctuation(clean_df$comment_content)
+
